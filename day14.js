babc15b1c1685c805b4a0dfb4e5ebd4931a3e462
snehal kamble <you@example.com>
2024-05-31 11:36:11 +0530

    added

---
+++ day14.js
@@ -0,0 +1,117 @@
+//array methods
+
+
+
+info = ["snehal","kamble",1254698546,24]
+console.log(info[0])
+
+//update
+
+info[0] = "nikita"
+console.log(info)
+
+//add
+
+info.push("pune")
+console.log(info)
+
+info.unshift("language")
+console.log(info)
+
+//delete
+
+info.pop()
+info.unshift()
+console.log(info)
+
+
+info2={
+      firstname:"snehal",
+      lastname:"kamble",
+      city:"mumbai",
+      age:25
+}
+//retirve
+info2.firstname
+console.log(info2)
+
+
+//update
+info2.firstname="nikita"
+console.log(info2)
+
+//add
+
+info2.city="pune"
+console.log(info2)
+
+//delete
+
+delete info2.firstname
+console.log(info2)
+
+
+
+//reverse
+
+name1=["snehal","sayli","chinmay","sarika"]
+let a1 = name1.reverse()
+console.log(a1)
+console.log(name1)
+
+
+///sort() /// sorting alphabetical in order
+
+
+name1.sort()
+console.log(name1)
+
+
+///join()
+
+fruits=["mango","banana","kiwi",515466400]
+
+let a2 = fruits.join("-")
+console.log(a2)
+
+
+///concat  -- join two diff array
+
+
+num=[11,54,6,66,85,78]
+num2=[55,85,14,22,34,58]
+let num3= num.concat(num2)
+console.log(num3)
+
+
+///slice (startIndex,endIndex(not included))
+
+
+city=["delhi","mumbai","pune","hinganghat","goa"]
+let q3=city.splice(1,3)
+
+console.log(q3)
+city.splice("pune","hinganghat")
+console.log(city)
+city.splice(1,3)
+console.log(city)
+
+
+///flat()
+
+num4=[[14,55],[22,66,77],[47,63]]
+console.log(num4[0,1])
+console.log(num4[2,2])
+
+let q5 = num4.flat()
+console.log(q5)
+
+
+
+
+
+
+
+
+
+
---
+++ day15.js
@@ -0,0 +1,149 @@
+///dot notation and bracket notation
+
+let info = {
+    firstname: "snehal",
+    lastname: "kmable",
+    age: 25,
+    city: "nagpur"
+
+}
+//dot notation
+
+//retrive
+console.log(info.firstname)
+console.log(info.city)
+
+//update
+
+info.firstname = "kartik"
+console.log(info)
+
+
+//add
+
+info.language = "English"
+console.log(info)
+
+//delete
+
+delete info.city
+console.log(info)
+
+///bracket notation
+
+//retrive
+
+console.log(info['firstname'])
+
+//update
+
+info['city'] = "mumbai"
+console.log(info)
+
+//add
+
+info['state'] = "maharshatra"
+console.log(info)
+
+//delete
+
+delete info['age']
+console.log(info)
+
+///----------------------------------------------------------------------
+
+
+///loop 
+//let names=["snehal","chinmay","sayli","kartiki","samiksha"]
+//console.log(names)
+// //loop on names
+// for (let i = 0;i < names.length;i++){
+//     console.log(names[i])
+//     console.log(i)
+// }
+
+
+///object -- loop
+
+// dot notation does not work inside loop
+
+let names = {
+    name: "snehal",
+    name2: "kartiki",
+    name3: "sayli",
+    name4: "vaishali"
+}
+for (let key in names) {
+    console.log(key)
+}
+
+for (let key in names) {
+    console.log(key, names[key])
+}
+
+for (let values in names) {
+    console.log(values, names[values])
+}
+
+
+for (let items in names) {
+    console.log(items, names[items])
+}
+
+
+
+/////program --
+
+let students = [
+    {
+        firstname: "chinmay",
+        lastname: "deshpande",
+        age:34,
+        city: "pune"
+
+    },
+    {
+        firstname: "kartiki",
+        lastname: "gharde",
+        age: 28,
+        city: "nagpur"
+
+    },
+    {
+        firstname: "niki",
+        lastname: "kamble",
+        age: 35,
+        city: "mumbai"
+
+    }
+]
+
+for(let i = 0;i<students.length;i++){
+    console.log(students[i].firstname +" "+students[i].lastname)
+}
+
+for(let i = 0;i <students.length;i++){
+students[i].language="Hindi"
+}
+console.log(students)
+
+
+///program ---
+
+let info1 = {
+    firstName:"snehal",
+    lastName:"kamble",
+    parent:{
+        mother:"jyotsna kamble",
+        lastName:"kawadu kamble"
+    }
+}
+
+info1.parent.mother="jyotsna k kamble"
+console.log(info1)
+
+
+console.log(info1.parent.mother)
+
+info1.parent.sister="niki"
+console.log(info1)
