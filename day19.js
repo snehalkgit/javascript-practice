23c73ef9c8b64c97810883588af207a745d37b33
snehal kamble <you@example.com>
2024-06-11 11:49:11 +0530

    added

--- day19.js
+++ day19.js
@@ -102,6 +102,104 @@ console.log(name.slice(2,-4))
 
  let city2 = "MUmbai"
  let q6 = city2.toUpperCase().toLowerCase().length
- console.log(q6)
+ console.log(q6) // return length  in last 
+ 
+
+//concat method
+
+
+let info1='i am travelling to mumbai'
+let a1=info1.concat(" and goa")
+console.log(a1)
+console.log(info1)
+///in concat  it does not ca=hnage original string 
+
+
+//reverse method
+
+let info2=["snehal","chinmay","sayli","mani"]
+info2.reverse()
+console.log(info2)
+let a4= info2.reverse()
+ //console.log(a4)
+ console.log(info2)
+ //in reverse method is doest not chnage in original sring 
+
+
+
+//sort method
+
+let name1= ["snehal","chinmay","sayli","mani"]
+name1.sort()
+console.log(name1)
+
+let a22= name1.sort()
+console.log(a22)
+ //it also does not chnge original arry
+
+
+
+//split method
+let info3 =  "snehal kamble 478555agsjjs"
+let splitA = info3.split('e')
+let splitb = info3.split('k')
+console.log(splitb)
+console.log(splitA)
+
+
+
+//startwith
+
+let info="snehal"
+let s1 =info.startsWith('s')
+console.log(s1)
+
+
+s2=info.endsWith('l')
+console.log(s2)
+console.log(typeof s2)
+
+
+//trim
+
+ let info4 = " mumbai "
+info4.trim()
+let d1= info4.trim()
+console.log(d1)
+console.log(info4)
+console.log(info4.length)
+
+
+
+let info5 = "mumbai "
+info5.trimEnd()
+console.log(info5)
+console.log(info5.length)
+
+
+let info6 = " mumbai"
+info6.trimStart()
+console.log(info6)
+console.log(typeof info6)
+console.log(info6.length)
+
+
+
+//chartat
+
+info='snehal'
+let d2= info.charAt('2')
+console.log(d2)
+
+console.log(d2.length)
+ let d3 = info.charCodeAt('4')
+console.log(d3)
+
+
+
+
+
+
+
 
 
